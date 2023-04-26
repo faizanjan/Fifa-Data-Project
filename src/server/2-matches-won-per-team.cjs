c25c36c54bcae310266e2c6184117533a0fe09f6
Faizan Jan <faizanjan619@gmail.com>
2023-04-26 15:02:20 +0530

    Fix the bug in server/2-matches-won-per-team.cjs, which counted the number of world-cups won by a team instead of number of matches won per team

--- src/server/2-matches-won-per-team.cjs
+++ src/server/2-matches-won-per-team.cjs
@@ -2,18 +2,37 @@ const csvToJson = require ('csvtojson');
 const fs = require('fs');
 
 const winnerFreq ={};
+
+// THE FOLLOWING COMMENTED CODE IS FOR COUNTING NUMBER OF WORLD-CUPS WON BY EACH TEAM
+// csvToJson()
+//     .fromFile('src/data/WorldCups.csv')
+//     .then(data=>{
+//         data.map(match=>{
+//             let winner = match.Winner;
+//             if(winner!==''){
+                // let freqKeys = Object.keys(winnerFreq);
+                // if(freqKeys.includes(winner)) winnerFreq[winner]++;
+                // else winnerFreq[winner]=1;
+//             }
+//         })
+//         // console.log(winnerFreq);
+        // fs.writeFileSync('src/public/output/2-matches-won-per-team.json', JSON.stringify(winnerFreq));
+//     });
+
 csvToJson()
-    .fromFile('src/data/WorldCups.csv')
+    .fromFile('src/data/WorldCupMatches.csv')
     .then(data=>{
         data.map(match=>{
-            let winner = match.Winner;
-            if(winner!==''){
-                let freqKeys = Object.keys(winnerFreq);
-                if(freqKeys.includes(winner)) winnerFreq[winner]++;
-                else winnerFreq[winner]=1;
+            let winner ='';
+            if(match['Home Team Goals']>match['Away Team Goals']) {
+                winner = match['Home Team Name']
             }
-        })
-        // console.log(winnerFreq);
-        fs.writeFileSync('src/public/output/2-matches-won-per-team.json', JSON.stringify(winnerFreq));
-    });
+            else if(match['Home Team Goals']<match['Away Team Goals'])winner = match['Away Team Name']
 
+            let freqKeys = Object.keys(winnerFreq);
+            if(freqKeys.includes(winner)) winnerFreq[winner]++;
+            else if (winner!=='') winnerFreq[winner]=1;
+        })
+        fs.writeFileSync('src/public/output/2-matches-won-per-team.json', JSON.stringify(winnerFreq));
+
+    })
