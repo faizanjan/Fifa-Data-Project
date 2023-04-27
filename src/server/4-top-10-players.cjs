1a7d85e5c238266bc78b04aa40f268100254653a
Faizan Jan <faizanjan619@gmail.com>
2023-04-27 10:11:53 +0530

    Fix the bug in 'server/4-top-10-players.cjs' which counts only 1st goal of the match by a player while ignoring the remaining goals (if any) that he scored.

--- src/server/4-top-10-players.cjs
+++ src/server/4-top-10-players.cjs
@@ -3,14 +3,24 @@ function top10Players(WorldCupPlayers) {
     WorldCupPlayers.map(match => {
         let event = match.Event;
         if (event.includes('G')) {
-            let index = event.indexOf('G');
-            if (event.charAt(index - 1) !== 'O') {
-                let freqKeys = Object.keys(top10);
-                if (freqKeys.includes(match['Player Name'])) {
-                    top10[match['Player Name']]++;
-                }
-                else top10[match['Player Name']] = 1;
+            let goals = 0;
+            for(let index= event.indexOf('G'); index<event.length; index++){
+                if(index===0 || (event.charAt(index)==='G' && event.charAt(index-1)!=='O')) goals++;
             }
+            let freqKeys = Object.keys(top10);
+                if (freqKeys.includes(match['Player Name'])) {
+                    top10[match['Player Name']]+=goals;
+                }
+                else top10[match['Player Name']] = goals;
+
+            // let index = event.indexOf('G');
+            // if (event.charAt(index - 1) !== 'O') {
+                // let freqKeys = Object.keys(top10);
+                // if (freqKeys.includes(match['Player Name'])) {
+                //     top10[match['Player Name']]++;
+                // }
+                // else top10[match['Player Name']] = 1;
+            // }
         }
     })
     let top10Arr = Object.entries(top10);
