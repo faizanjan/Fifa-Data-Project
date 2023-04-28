1211ce8a11f1233cb2cd35e45f518b32f52ca620
Faizan Jan <faizanjan619@gmail.com>
2023-04-28 17:49:32 +0530

    Add a check i mapObject.cjs to see if the callback is really a function

--- src/server/mapObject.cjs
+++ src/server/mapObject.cjs
@@ -1,5 +1,6 @@
 function mapObject(obj, callback) {
     if (!checkArg(obj)) return {};
+    if(typeof(callback)!=='function') throw new Error("Invalid Callback")
     let newObject = {};
     for (let key in obj){
         newObject[key]=callback(obj[key],key);
