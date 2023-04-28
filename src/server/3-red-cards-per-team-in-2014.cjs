183b2fa734e8328c23fc61950548ab8d19d73409
Faizan Jan <faizanjan619@gmail.com>
2023-04-28 11:23:11 +0530

    "Modify server/3-red-cards-per-team-in-2014.cjs" - Make getMatchForYear function to return an object instead of array - It'll reduce complexity of redCardsPerTeamInAYear by avoiding using .includes method

--- src/server/3-red-cards-per-team-in-2014.cjs
+++ src/server/3-red-cards-per-team-in-2014.cjs
@@ -1,9 +1,9 @@
 
 function getMatchsForYear (WorldCupMatches,year){
     const idList = WorldCupMatches.reduce((acc,match)=>{
-        if(parseInt(match.Year)===year) acc.push(match.MatchID)
+        if(parseInt(match.Year)===year) acc[match.MatchID]=true;
         return acc;
-    },[])
+    },{})
     return idList;
 }
 
@@ -14,7 +14,7 @@ function redCardsPerTeamInAYear(WorldCupMatches,WorldCupPlayers, year){
 
     // Get red cards in the specified year by comparing the matchIDs with above array 
     const redCardsPerTeam = WorldCupPlayers.reduce((acc,match)=>{
-        if(matchIdsForAYear.includes(match.MatchID)){
+        if(matchIdsForAYear[match.MatchID]){
             let event = match.Event;
             if(event.includes('R')){
                 if(acc[match['Team Initials']]) {
