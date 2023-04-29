cd48b162128fabe9713f38614217956343fac030
Faizan Jan <faizanjan619@gmail.com>
2023-04-29 11:17:04 +0530

    Remove the Bug from src/server/2-matches-won-per-team.cjs which made the function to throw error instead of skipping entry if data if empty

--- src/server/2-matches-won-per-team.cjs
+++ src/server/2-matches-won-per-team.cjs
@@ -4,6 +4,7 @@ function matchesWonPerTeam (WorldCupMatches){
     }
     const winnerFreq = WorldCupMatches.reduce ((acc, match)=>{
         let winner;
+        if(match['MatchID']==='') return acc;
         let homeGoals=match['Home Team Goals'], awayGoals=match['Away Team Goals'];
         if(parseInt(homeGoals)!=homeGoals || parseInt(awayGoals)!=awayGoals){
             throw new Error("Number of goals are not valid");
@@ -14,7 +15,7 @@ function matchesWonPerTeam (WorldCupMatches){
         else if(homeGoals<awayGoals)winner = match['Away Team Name']
         if(typeof(winner)==='string'){
             if(acc[winner]) acc[winner]++;
-            else if (winner!=='') acc[winner]=1;
+            else acc[winner]=1;
         }
         return acc;
     },{})
