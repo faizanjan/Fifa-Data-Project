f6fe3d95c10e9c0172ef642d5a3232922ecdd211
Faizan Jan <faizanjan619@gmail.com>
2023-04-29 14:45:22 +0530

    Cover the corner cases in src/server/5-top-players-by-avg-goals.cjs

--- src/server/5-top-players-by-avg-goals.cjs
+++ src/server/5-top-players-by-avg-goals.cjs
@@ -1,6 +1,9 @@
 const mapObject = require('./mapObject.cjs');
 
 function top10Players(WorldCupPlayers) {
+    if(!Array.isArray(WorldCupPlayers)) {
+        throw new Error("Invalid Input Type");
+    }
     let goalsByPlayers = getGoalsByPlayers(WorldCupPlayers);
     let matchesCount = getMatchesPlayedByPlayers(WorldCupPlayers);
 
@@ -15,8 +18,12 @@ function top10Players(WorldCupPlayers) {
 }
 
 function getGoalsByPlayers(WorldCupPlayers) {
+    if(!Array.isArray(WorldCupPlayers)) {
+        throw new Error("Invalid Input Type");
+    }
     let goalsByPlayers = WorldCupPlayers.reduce((acc, match) => {
         let event = match.Event;
+        if(event===undefined) return acc;
         if (event.includes('G')) {
             let goals = 0;
             for (let index = event.indexOf('G'); index < event.length; index++) {
@@ -33,12 +40,19 @@ function getGoalsByPlayers(WorldCupPlayers) {
 }
 
 function getMatchesPlayedByPlayers(WorldCupPlayers) {
+    if(!Array.isArray(WorldCupPlayers)) {
+        throw new Error("Invalid Input Type");
+    }
     let matchesCount = WorldCupPlayers.reduce((acc,match)=>{
-        if(acc[match['Player Name']]) acc[match['Player Name']]++;
-        else acc[match['Player Name']]=1;
-        return acc; 
+        let name = match['Player Name'];
+        if (name===undefined) return acc;
+        else {
+            if(acc[name]) acc[name]++;
+            else acc[name]=1;
+            return acc; 
+        }
     },{})
     return matchesCount
 }
 
-module.exports = top10Players;
+module.exports = {top10Players, getGoalsByPlayers, getMatchesPlayedByPlayers};
