039b949c5b1650496f162b138cb9be5ff76bdcf3
Faizan Jan <faizanjan619@gmail.com>
2023-04-29 14:43:50 +0530

    Change the require statements to implement destructuring as the modules changed the content of exports to multiple functions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const csvToJson = require ('csvtojson');
 const fs = require('fs');
 const matchesPerCity = require('./server/1-matches-played-per-city.cjs');
 const matchesWonPerTeam = require('./server/2-matches-won-per-team.cjs');
-const redCardsPerTeamInAYear = require('./server/3-red-cards-per-team-in-2014.cjs');
-const top10Players = require('./server/4-top-10-players.cjs');
+const {redCardsPerTeamInAYear} = require('./server/3-red-cards-per-team-in-2014.cjs');
+const {top10Players} = require('./server/5-top-players-by-avg-goals.cjs');
 
 // Retrieve the CSV data in JSON format for a file in argumented path
 async function getDataFromCSV(path){
@@ -36,8 +36,8 @@ async function getDataFromCSV(path){
     fs.writeFileSync('src/public/output/3-red-cards-per-team-in-2014.json', JSON.stringify(redCardsPerTeam));
 })();
 
-// Problem 4:
-(async function prob4(){
+// Problem 4/5:
+(async function prob5(){
     let WorldCupPlayers = await getDataFromCSV('src/data/WorldCupPlayers.csv');
     const top10 = top10Players(WorldCupPlayers);
     // console.log(top10);
