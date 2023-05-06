aa87db9b9bae8fe16981ca067698138ac2624583
Faizan Jan <faizanjan619@gmail.com>
2023-05-06 11:51:44 +0530

    Change the query of problem 5 to select only those players who have never gotten any red or yellow card

--- src/server/5-most-matches-without-cards.cjs
+++ src/server/5-most-matches-without-cards.cjs
@@ -6,16 +6,31 @@ const client = new Client(clientInfo);
 
 client.connect();
 
+// SELECT withoutcards."Player Name" AS "Player Name", COUNT(withoutcards."Matches Played") AS "Matches" 
 let events = client.query(`
-    SELECT "Player Name", COUNT("MatchID") AS "Matches Played"
-    FROM "WorldCupPlayers"
-    WHERE "Event" NOT LIKE '%R%' AND "Event" NOT LIKE '%Y%'
-    GROUP BY "Player Name"
-    ORDER BY "Matches Played" DESC,
-             "Player Name" ASC
-    LIMIT 10
-`)
-
+SELECT *
+    FROM 
+        (SELECT "Player Name", COUNT("MatchID") AS "Matches Played"
+        FROM "WorldCupPlayers"
+        WHERE "Event" NOT LIKE '%R%' AND "Event" NOT LIKE '%Y%'
+        GROUP BY "Player Name") 
+        AS withoutcards
+    JOIN
+        (SELECT "Player Name", COUNT("MatchID") AS "Matches Played"
+        FROM "WorldCupPlayers"
+        GROUP BY "Player Name") 
+        AS withcards
+    ON withoutcards."Player Name" = withcards."Player Name"
+    WHERE withoutcards."Matches Played" = withcards."Matches Played"
+    ORDER BY withoutcards."Matches Played" DESC,
+             withoutcards."Player Name" ASC
+    LIMIT 10;
+        `)
+        // WHERE withcards."Matches Played" = withcards."Matches Played"
+        // GROUP BY withoutcards."Player Name"
+        // ORDER BY withoutcards."Matches Played" DESC,
+        //          withoutcards."Player Name" ASC
+        // LIMIT 10
 events
     .then((res) => {
         return fs.writeFile(path.join(__dirname, '../public/output/5-most-matches-without-cards.json'), JSON.stringify(res.rows));
