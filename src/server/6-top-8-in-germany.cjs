e1658ad3ebb0c11011bbccfbd90bfdbe3f267192
Faizan Jan <faizanjan619@gmail.com>
2023-05-06 17:53:58 +0530

    Create a module to Find the top 8 teams that has scored the most number of goals in Germany

---
+++ src/server/6-top-8-in-germany.cjs
@@ -0,0 +1,34 @@
+const {Client} = require('pg');
+const fs = require('fs').promises;
+const clientInfo= require('../clientInfo');
+const path = require('path');
+const client = new Client(clientInfo);
+
+client.connect();
+
+let result = client.query(`
+SELECT "Team Name", SUM ("Goals") AS "Goals" 
+FROM 
+    (
+        SELECT "Home Team Name" AS "Team Name" ,  
+            "Home Team Goals" AS "Goals" , "Year"
+        FROM "WorldCupMatches" 
+        UNION ALL 
+        SELECT "Away Team Name" AS "Team Name",  
+            "Away Team Goals" AS "Goals" , "Year"
+        FROM "WorldCupMatches" 
+    ) AS sub 
+WHERE "Year" IN 
+    (SELECT "Year" from "WorldCups" where "Country" = 'Germany')
+GROUP BY "Team Name"
+ORDER BY "Goals" DESC
+LIMIT 8;
+`)
+
+result 
+    .then((res)=>{
+        // console.log(res.rows);
+        return fs.writeFile(path.join(__dirname, '../public/output/6-top-8-in-germany.json'), JSON.stringify(res.rows));
+    })
+    .catch(err=>console.error(err))
+    .finally(()=>{client.end()})
