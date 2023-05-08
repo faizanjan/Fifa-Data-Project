41835151fcb524ca93561f122250813ba30f2c1d
Faizan Jan <faizanjan619@gmail.com>
2023-05-08 12:50:01 +0530

    Make a utility function to execute the queries and write the outputs of all the problem. Use this utility module for all the problems

---
+++ src/server/util.cjs
@@ -0,0 +1,21 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { Client } = require("pg");
+const clientInfo = require("../clientInfo");
+const client = new Client(clientInfo);
+
+module.exports = function sqlTrigger (query, outputPath){
+  client.connect();
+  client
+    .query(query)
+    .then((res) => {
+      return fs.writeFile(
+        path.join(__dirname, outputPath),
+        JSON.stringify(res.rows)
+      );
+    })
+    .catch((err) => console.error(err))
+    .finally(() => {
+      client.end();
+    });
+};
